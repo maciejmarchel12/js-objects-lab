ae9f015220af7290439e3ccea6e85f17067325d6
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-09-12 10:29:39 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -9,6 +9,7 @@ const aCar = {
         model : 'Corolla',
         cc : 1.8
     },
+    features: ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : {
         year: 201,
         county: 'WD',
@@ -36,3 +37,15 @@ console.log(
   );
 
   console.log('First owner : ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address);
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+  }
+
+  for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name)
+  }
+
+  for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
